d74d9a4911a35b88dda9f0e2d0c76c1ecd346adf
plein <68096727+planetoryd@users.noreply.github.com>
2024-01-05 00:05:57 +0800

    add scripts. include go checksums in git.

---
+++ daemon/readme.md
@@ -0,0 +1,4 @@
+
+requires 
+
+- libnetfilter_queue
---
+++ proto/prereqs.sh
@@ -0,0 +1,5 @@
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
+python -m venv ../venv
+../venv/bin/pip install grpcio-tools
